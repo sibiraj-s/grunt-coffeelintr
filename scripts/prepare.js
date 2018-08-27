98df9df41f5a232a2a53e6ffad0beae0a0229950
Sibiraj <dev.sibiraj@outlook.com>
2018-08-27 14:29:06 +0530

    style: use two space indentation instead of four

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,18 +2,17 @@
 
 const fs = require('fs');
 
-fs.readFile('dist/package.json', 'utf8', function (err, data) {
+fs.readFile('dist/package.json', 'utf8', function (readErr, data) {
+  if (readErr) throw readErr;
 
-    if (err) throw err;
-    const pkg = JSON.parse(data);
-    pkg['main'] = 'tasks/coffeelintr.js';
-    delete pkg['scripts'];
-    delete pkg['devDependencies'];
-    delete pkg['private'];
+  const pkg = JSON.parse(data);
+  pkg.main = 'tasks/coffeelintr.js';
+  delete pkg.scripts;
+  delete pkg.devDependencies;
+  delete pkg.private;
 
-    fs.writeFile('dist/package.json', JSON.stringify((pkg), null, 2), 'utf8', function (err) {
-        if (err) throw err;
-        console.log('postbuild: package.json written');
-    });
-
+  fs.writeFile('dist/package.json', JSON.stringify((pkg), null, 2), 'utf8', function (writeErr) {
+    if (writeErr) throw writeErr;
+    console.log('postbuild: package.json written');
+  });
 });
--- tasks/coffeelintr.js
+++ tasks/coffeelintr.js
@@ -4,9 +4,7 @@ const coffeelint = require('coffeelint');
 const reporter = require('coffeelint-reporter').reporter;
 
 module.exports = function (grunt) {
-
   grunt.registerMultiTask('coffeelintr', 'A Grunt Plugin to lint Cofeescrript', function () {
-
     const files = this.filesSrc;
     const options = this.options();
     let coffeelintOptions = {};
@@ -27,18 +25,14 @@ module.exports = function (grunt) {
     }
 
     files.forEach(function (fileName) {
-
       const lintResults = coffeelint.lint(grunt.file.read(fileName), coffeelintOptions);
 
       reporter(fileName, lintResults);
 
       lintResults.forEach(function (error) {
-
         if (error.level === 'error') errorCount += 1;
         else if (error.level === 'warn') warningCount += 1;
-
       });
-
     });
 
     if (errorCount && !options.force) return false;
@@ -46,7 +40,5 @@ module.exports = function (grunt) {
     if (!warningCount && !errorCount) {
       grunt.log.ok(files.length + ' file' + (files.length === 1 ? '' : 's') + ' lint free.');
     }
-
   });
-
 };
